f61cc7eb9c44c95a27891e214ed7fb36a59f9344
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-12-24 14:31:29 -0300

    End Point post

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -35,7 +35,8 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $marca =Marca::create($request->all());
+         return $marca;
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\LocacaoController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\ModeloController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +22,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/', function () {
-    return ['Chegamos ate aqui' => 'Sim'];
-});
+//chama todos as funções de um controller (API)
+Route::apiResource('cliente', ClienteController::class);
+Route::apiResource('carro', CarroController::class);
+Route::apiResource('locacao', LocacaoController::class);
+Route::apiResource('marca', MarcaController::class);
+Route::apiResource('modelo', ModeloController::class);
