8c3639886c2ffd6185f8e655bb8e40422cf8f81c
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-12-28 14:50:33 -0300

    Validando parametros e Accept

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -43,6 +43,19 @@ class MarcaController extends Controller
     public function store(Request $request)
     {
         //$marca =Marca::create($request->all());
+        //validando parametros nome e imagem
+        $regras = [
+            'nome' => 'required|unique:marcas',
+            'imagem' => 'required'
+        ];
+
+        $feedback = [
+            'required' => 'O campo é obrigatório',
+            'nome.unique' => 'O nome da marca já existe'
+        ];
+
+        $request->validate($regras, $feedback);
+
         $marca = $this->marca->create($request->all());
          return response()->json($marca, 201);
     }
